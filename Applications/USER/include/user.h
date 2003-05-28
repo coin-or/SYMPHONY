c38ad329b0e721b6c7bf96210d6f343a3d58bd17
Ted Ralphs <ted@lehigh.edu>
2003-05-28 17:57:24 +0000

    *** empty log message ***

---
+++ Applications/USER/include/user.h
@@ -0,0 +1,38 @@
+/*===========================================================================*/
+/*                                                                           */
+/* This file is part of the SYMPHONY Branch, Cut, and Price Library.         */
+/*                                                                           */
+/* SYMPHONY was jointly developed by Ted Ralphs (tkralphs@lehigh.edu) and    */
+/* Laci Ladanyi (ladanyi@us.ibm.com).                                        */
+/*                                                                           */
+/* (c) Copyright 2000, 2001, 2002 Ted Ralphs. All Rights Reserved.           */
+/*                                                                           */
+/* This software is licensed under the Common Public License. Please see     */
+/* accompanying file for terms.                                              */
+/*                                                                           */
+/*===========================================================================*/
+
+#ifndef _USER_H
+#define _USER_H
+
+#define MAX_FILE_NAME_LENGTH 100
+
+/*---------------------------------------------------------------------------*\
+ * 
+\*---------------------------------------------------------------------------*/
+
+typedef struct USER_PARAMETERS{
+   char             infile[MAX_FILE_NAME_LENGTH + 1];
+}user_parameters;
+
+/*---------------------------------------------------------------------------*\
+ * 
+\*---------------------------------------------------------------------------*/
+
+typedef struct USER_PROBLEM{
+   int              colnum;
+   int              rownum;
+   user_parameters  par;
+}user_problem;
+
+#endif
