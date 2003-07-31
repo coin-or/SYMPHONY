266c434401bb8623fd2ecfbc560f28d90c8c4675
Ted Ralphs <ted@lehigh.edu>
2003-07-31 02:11:44 +0000

    *** empty log message ***

--- src/LP/lp_solver.c
+++ src/LP/lp_solver.c
@@ -2165,8 +2165,8 @@ void get_slacks(LPdata *lp_data)
 
 #else
    
-   CPXgetslack(const_cast<OsiCpxSolverInterface *>(lp_data->si)->getEnvironmentPtr,
-	       const_cast<OsiCpxSolverInterface *>(lp_data->si)->getLpPtr,
+   CPXgetslack(dynamic_cast<OsiCpxSolverInterface *>(lp_data->si)->getEnvironmentPtr,
+	       dynamic_cast<OsiCpxSolverInterface *>(lp_data->si)->getLpPtr,
 	       lp_data->slacks, 0, lp_data->m-1);
    /* Compute the real slacks for the free rows */
    for (i =m - 1; i >= 0; i--){
