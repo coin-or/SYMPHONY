f177866311f862e8a233b6a5c5e182209112b4d6
Ted Ralphs <ted@lehigh.edu>
2004-03-02 05:12:43 +0000

    Adding main.c

---
+++ src/Master/main.c
@@ -0,0 +1,67 @@
+/*===========================================================================*/
+/*                                                                           */
+/* This file is part of the SYMPHONY Branch, Cut, and Price Library.         */
+/*                                                                           */
+/* SYMPHONY was jointly developed by Ted Ralphs (tkralphs@lehigh.edu) and    */
+/* Laci Ladanyi (ladanyi@us.ibm.com).                                        */
+/*                                                                           */
+/* (c) Copyright 2000-2003 Ted Ralphs. All Rights Reserved.                  */
+/*                                                                           */
+/* This software is licensed under the Common Public License. Please see     */
+/* accompanying file for terms.                                              */
+/*                                                                           */
+/*===========================================================================*/
+
+#define COMPILING_FOR_MASTER
+
+/*===========================================================================*/
+
+/*===========================================================================*\
+ * This file contains the main() for the master process.
+\*===========================================================================*/
+
+#if defined(USE_OSI_INTERFACE) && !defined(USER_MAIN) 
+
+#include "OsiSymSolverInterface.hpp"
+
+int main(int argc, char **argv)
+{
+   OsiSymSolverInterface si;
+
+   /* Parse the command line */
+   si.parseCommandLine(argc, argv);
+   
+   /* Read in the problem */
+   si.loadProblem();
+
+   /* Find a priori problem bounds */
+   si.findInitialBounds();
+
+   /* Solve the problem */
+   si.branchAndBound();
+   
+   return(0);
+}
+
+#elif !defined(USER_MAIN)
+
+#include "master.h"
+
+int main(int argc, char **argv)
+{
+   problem *p = sym_open_environment();
+
+   sym_parse_command_line(p, argc, argv);
+
+   sym_load_problem(p);
+
+   sym_find_initial_bounds(p);
+
+   sym_solve(p);
+
+   sym_close_environment(p);
+
+   return(0);
+}
+
+#endif
