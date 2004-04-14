e29cfb7ebc06a5ece73962ae01de9e6e80145ad2
Ted Ralphs <ted@lehigh.edu>
2004-04-14 03:37:17 +0000

    Adding vrp_main.c

---
+++ Applications/VRP/src/Master/vrp_main.c
@@ -0,0 +1,70 @@
+/*===========================================================================*/
+/*                                                                           */
+/* This file is part of a demonstration application for use with the         */
+/* SYMPHONY Branch, Cut, and Price Library. This application is a solver for */
+/* the Vehicle Routing Problem and the Traveling Salesman Problem.           */
+/*                                                                           */
+/* (c) Copyright 2000-2003 Ted Ralphs. All Rights Reserved.                  */
+/*                                                                           */
+/* This application was developed by Ted Ralphs (tkralphs@lehigh.edu)        */
+/*                                                                           */
+/* This software is licensed under the Common Public License. Please see     */
+/* accompanying file for terms.                                              */
+/*                                                                           */
+/*===========================================================================*/
+
+#define COMPILING_FOR_MASTER
+
+/*===========================================================================*/
+
+/*===========================================================================*\
+ * This file contains the main() for the master process.
+\*===========================================================================*/
+
+#if 1
+
+#include "OsiSymSolverInterface.hpp"
+
+int main(int argc, char **argv)
+{
+   OsiSymSolverInterface si;
+
+   si.setSymParam(OsiSymVerbosity, 3);
+
+   /* Parse the command line */
+   si.parseCommandLine(argc, argv);
+   
+   /* Read in the problem */
+   si.loadProblem();
+
+   /* Find a priori problem bounds */
+   si.findInitialBounds();
+
+   /* Solve the problem */
+   si.branchAndBound();
+
+   return(0);
+}
+
+#else
+
+#include "master.h"
+
+int main(int argc, char **argv)
+{
+   problem *p = sym_open_environment();
+
+   sym_parse_command_line(p, argc, argv);
+
+   sym_load_problem(p);
+
+   sym_find_initial_bounds(p);
+
+   sym_solve(p);
+
+   sym_close_environment(p);
+
+   return(0);
+}
+
+#endif
