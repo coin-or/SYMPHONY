98c211b344b05d73c98dac3c10e8203d8fc70d89
Ted Ralphs <ted@lehigh.edu>
2005-02-22 17:09:26 +0000

    Minor bug fixes

--- Applications/VRP/include/vrp_types.h
+++ Applications/VRP/include/vrp_types.h
@@ -150,10 +150,10 @@ typedef struct VRP_PARAMS{
    int           tours_to_keep;
    time_out_par  time_out;
    int           do_heuristics;
+#endif
 
    int           test;   
    char          test_dir[MAX_FILE_NAME_LENGTH +1]; /* Test files directory */ 
-#endif
 }vrp_params;
 
 /*---------------------------------------------------------------------------*\
