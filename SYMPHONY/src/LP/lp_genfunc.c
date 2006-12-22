d331e0325ca0e63bc012dfea0e7265d181f6294a
Ted Ralphs <ted@lehigh.edu>
2006-12-22 21:33:30 +0000

    Undoing last commit

--- SYMPHONY/src/LP/lp_genfunc.c
+++ SYMPHONY/src/LP/lp_genfunc.c
@@ -297,7 +297,7 @@ int fathom_branch(lp_prob *p)
 	 }
 	 if (termcode == LP_D_UNBOUNDED){
 	    PRINT(p->par.verbosity, 1, ("Feasibility lost -- "));
-#if 1
+#if 0
 	    char name[50] = "";
 	    sprintf(name, "matrix.%i.%i", p->bc_index, p->iter_num);
 	    write_mps(lp_data, name);
