05a13a90a0890752af1e83a5a072a4f268c92099
Ashutosh Mahajan <amahajan@iitb.ac.in>
2007-05-01 01:17:46 +0000

    gladly restored Doc and Examples. phew.

---
+++ SYMPHONY/Examples/milp.c
@@ -0,0 +1,59 @@
+/*===========================================================================*/
+/*                                                                           */
+/* This file is part of the SYMPHONY MILP Solver Framework.                  */
+/*                                                                           */
+/* SYMPHONY was jointly developed by Ted Ralphs (tkralphs@lehigh.edu) and    */
+/* Laci Ladanyi (ladanyi@us.ibm.com).                                        */
+/*                                                                           */
+/* The author of this file is Menal Guzelsoy                                 */
+/*                                                                           */
+/* (c) Copyright 2005-2007 Lehigh University. All Rights Reserved.           */
+/*                                                                           */
+/* This software is licensed under the Common Public License. Please see     */
+/* accompanying file for terms.                                              */
+/*                                                                           */
+/*===========================================================================*/
+
+#ifdef USE_OSI_INTERFACE
+
+#include "OsiSymSolverInterface.hpp"
+
+int main(int argc, char **argv)
+{
+
+   /* Create an OsiSym object */
+   OsiSymSolverInterface si;
+
+   /* Parse the command line */
+   si.parseCommandLine(argc, argv);
+   
+   /* Read in the problem */
+   si.loadProblem();
+
+   /* Solve the problem */
+   si.branchAndBound();
+
+   return(0);
+}
+
+#else
+
+#include "symphony.h"
+  
+int main(int argc, char **argv)
+{    
+     
+   sym_environment *env = sym_open_environment();
+   
+   sym_parse_command_line(env, argc, argv);
+   
+   sym_load_problem(env);
+   
+   sym_solve(env);
+   
+   sym_close_environment(env);
+  
+   return(0);
+}  
+
+#endif
---
+++ SYMPHONY/Examples/warm_start1.c
@@ -0,0 +1,72 @@
+/*===========================================================================*/
+/*                                                                           */
+/* This file is part of the SYMPHONY MILP Solver Framework.                  */
+/*                                                                           */
+/* SYMPHONY was jointly developed by Ted Ralphs (tkralphs@lehigh.edu) and    */
+/* Laci Ladanyi (ladanyi@us.ibm.com).                                        */
+/*                                                                           */
+/* The author of this file is Menal Guzelsoy                                 */
+/*                                                                           */
+/* (c) Copyright 2005-2007 Lehigh University. All Rights Reserved.           */
+/*                                                                           */
+/* This software is licensed under the Common Public License. Please see     */
+/* accompanying file for terms.                                              */
+/*                                                                           */
+/*===========================================================================*/
+
+#ifdef USE_OSI_INTERFACE
+
+#include "OsiSymSolverInterface.hpp"
+
+int main(int argc, char **argv)
+{
+
+  OsiSymSolverInterface si;
+  si.parseCommandLine(argc, argv);
+  si.loadProblem();
+  si.setSymParam(OsiSymKeepWarmStart, true);
+  si.setSymParam(OsiSymFindFirstFeasible, true);
+  si.setSymParam(OsiSymSearchStrategy, DEPTH_FIRST_SEARCH);
+
+  si.initialSolve();
+
+  si.setSymParam(OsiSymFindFirstFeasible, false);
+  si.setSymParam(OsiSymSearchStrategy, BEST_FIRST_SEARCH);
+
+  si.resolve();
+  
+  return(0);
+
+}
+
+#else
+
+#include "symphony.h"
+  
+int main(int argc, char **argv)
+{    
+     
+   sym_environment *env = sym_open_environment();   
+   sym_parse_command_line(env, argc, argv);   
+   sym_load_problem(env);
+   
+   sym_set_int_param(env, "keep_warm_start", TRUE);
+
+   sym_set_int_param(env, "find_first_feasible", TRUE);
+   sym_set_int_param(env, "node_selection_rule", DEPTH_FIRST_SEARCH);
+
+   sym_solve(env);
+
+   sym_set_int_param(env, "find_first_feasible", FALSE);
+   sym_set_int_param(env, "node_selection_rule", BEST_FIRST_SEARCH);
+
+
+   sym_warm_solve(env);
+   sym_close_environment(env);
+  
+   return(0);
+
+}  
+
+#endif
+
