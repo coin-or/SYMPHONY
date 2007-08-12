1e40a46f6501da64fa40d00c3e92c80ed184c0b1
Stefan Vigerske <svigerske@gams.com>
2007-08-12 13:32:27 +0000

    change 'if (a=b)' to 'if ((a=b))' to avoid compiler warning

--- trunk/SYMPHONY/src/Master/master.c
+++ trunk/SYMPHONY/src/Master/master.c
@@ -941,7 +941,7 @@ int sym_solve(sym_environment *env)
    env->warm_start->stat = tm->stat;
    env->warm_start->phase = tm->phase;
    env->warm_start->lb = tm->lb;
-   if (env->warm_start->has_ub = tm->has_ub){
+   if ((env->warm_start->has_ub = tm->has_ub)!=0){
       env->warm_start->ub = tm->ub;
    }
    env->par.tm_par.warm_start = FALSE;
@@ -1377,7 +1377,7 @@ int sym_mc_solve(sym_environment *env)
    /* Solve */
    env->utopia[0] = 0;
    env->utopia[1] = -MAXINT;
-   if (termcode = sym_solve(env) < 0){
+   if ((termcode = sym_solve(env) < 0)){
       env->base->cutnum -=2;
       env->rootdesc->uind.size--;
       return(termcode);
@@ -1442,7 +1442,7 @@ int sym_mc_solve(sym_environment *env)
 
       if (env->par.mc_warm_start && env->par.mc_warm_start_rule == 0){
 	 sym_set_warm_start(env, ws);
-	 if (termcode = sym_warm_solve(env) < 0){
+	 if ((termcode = sym_warm_solve(env) < 0)){
 	    switch(env->par.mc_warm_start_rule){
 	     case 0:
 		sym_delete_warm_start(ws);
@@ -1455,12 +1455,12 @@ int sym_mc_solve(sym_environment *env)
 	    return(termcode);
 	 }
       }else{
-	 if (termcode = sym_solve(env) < 0){
+	 if ((termcode = sym_solve(env) < 0)){
 	    return(termcode);
 	 }
       }
    }else{
-      if (termcode = sym_solve(env) < 0){
+      if ((termcode = sym_solve(env) < 0)){
 	 env->base->cutnum -=2;
 	 env->rootdesc->uind.size--;
 	 return(termcode);
@@ -1656,7 +1656,7 @@ int sym_mc_solve(sym_environment *env)
 		break;
 	    }
 	    
-	    if (termcode = sym_warm_solve(env) < 0){
+	    if ((termcode = sym_warm_solve(env) < 0)){
 
 	       /* FIXME! copy best_sol.xind and .xval from env to warm_start*/
 	       memset(&(env->best_sol), 0, sizeof(lp_sol));
@@ -1690,12 +1690,12 @@ int sym_mc_solve(sym_environment *env)
 	       return(termcode);
 	    }
 	 }else{
-	    if (termcode = sym_solve(env) < 0){
+	    if ((termcode = sym_solve(env) < 0)){
 	       return(termcode);
 	    }
 	 }
       } else{
-	 if (termcode = sym_solve(env) < 0){
+	 if ((termcode = sym_solve(env) < 0)){
 	    env->base->cutnum -=2;
 	    env->rootdesc->uind.size--;
 	    return(termcode);
@@ -5707,12 +5707,12 @@ int sym_test(sym_environment *env)
        sprintf(infile, "%s%s%s", mps_dir, "/", mps_files[i]);
     else
        sprintf(infile, "%s%s%s", mps_dir, "\\", mps_files[i]);   
-    if( termcode = sym_read_mps(env, infile) < 0)
+    if( (termcode = sym_read_mps(env, infile) < 0) )
       return(termcode);
 
     printf("\nSolving %s...\n\n", mps_files[i]);
     
-    if(termcode = sym_solve(env) < 0 )
+    if((termcode = sym_solve(env) < 0) )
       return(termcode);
 
     sym_get_obj_val(env, &obj_val[i]);
