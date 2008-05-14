17dc7903f6547ca3ca1ff8b7a2b5762526c54039
Ted Ralphs <ted@lehigh.edu>
2008-05-14 18:40:06 +0000

    Updating documentation and copyright notices

--- SYMPHONY/Applications/MPP/include/mpp.h
+++ SYMPHONY/Applications/MPP/include/mpp.h
@@ -4,7 +4,7 @@
 /* SYMPHONY Branch, Cut, and Price Library. This application is a solver for */
 /* the Mixed Postman Problem.                                                */
 /*                                                                           */
-/* (c) Copyright 2005-2007 Lehigh University. All Rights Reserved.           */
+/* (c) Copyright 2005-2008 Lehigh University. All Rights Reserved.           */
 /*                                                                           */
 /* This application was originally developed by Andrew Hofmann and was       */
 /* modified by  Ted Ralphs (tkralphs@lehigh.edu)                             */
--- SYMPHONY/test/unitTestSym.cpp
+++ SYMPHONY/test/unitTestSym.cpp
@@ -5,7 +5,7 @@
 /* SYMPHONY was jointly developed by Ted Ralphs (tkralphs@lehigh.edu) and    */
 /* Laci Ladanyi (ladanyi@us.ibm.com).                                        */
 /*                                                                           */
-/* (c) Copyright 2006-2007 Lehigh University. All Rights Reserved.           */
+/* (c) Copyright 2006-2008 Lehigh University. All Rights Reserved.           */
 /*                                                                           */
 /* This software is licensed under the Common Public License. Please see     */
 /* accompanying file for terms.                                              */
