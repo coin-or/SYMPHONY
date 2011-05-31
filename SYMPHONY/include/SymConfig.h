6a7942b29907eaa1ef33118aa77b8e25f6b0c9e0
Ted Ralphs <ted@lehigh.edu>
2011-05-31 13:53:32 +0000

    Updates to configuration header file system

--- SYMPHONY/include/SymConfig.h
+++ SYMPHONY/include/SymConfig.h
@@ -21,79 +21,20 @@
 #define __SYMPHONY_CONFIG_H__
 
 #ifdef HAVE_CONFIG_H
+#ifdef SYMPHONY_BUILD
+#include "config.h"
+#else
 #include "config_sym.h"
-
-/* undefine macros that could conflict with those in other config.h
-   files */
-#undef PACKAGE
-#undef PACKAGE_BUGREPORT
-#undef PACKAGE_NAME
-#undef PACKAGE_STRING
-#undef PACKAGE_TARNAME
-#undef PACKAGE_VERSION
-#undef VERSION
+#endif
 
 #else /* HAVE_CONFIG_H */
 
-/* include the COIN-wide system specific configure header */
-#include "configall_system.h"
-
-/***************************************************************************/
-/*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
-/***************************************************************************/
-
-/* If defined, debug sanity checks are performed during runtime */
-/* #define COIN_DEBUG 1 */
-
-/* Define to 1 if the Cbc package is used */
-/* #define COIN_HAS_CBC 1 */
-
-/* Define to 1 if the Cgl package is used */
-#define COIN_HAS_CGL 1
-
-/* Define to 1 if the Clp package is used */
-#define COIN_HAS_CLP 1
-
-/* Define to 1 if the CoinUtils package is used */
-#define COIN_HAS_COINUTILS 1
-
-/* Define to 1 if the Osi package is used */
-#define COIN_HAS_OSI 1
-
-/* Define to 1 if the Vol package is used */
-/* #define COIN_HAS_VOL 1 */
-
-/* Define to 1 if the Cplex package is used */
-/* #define COIN_HAS_CPX 1 */
-
-/* Define to 1 if the Dylp package is used */
-/* #undef COIN_HAS_DYLP */
-
-/* Define to 1 if the FortMP package is used */
-/* #undef COIN_HAS_FMP */
-
-/* Define to 1 if the Glpk package is used */
-/* #undef COIN_HAS_GLPK */
-
-/* Define to 1 if the Mosek package is used */
-/* #undef COIN_HAS_MSK */
-
-/* Define to 1 if the Osl package is used */
-/* #undef COIN_HAS_OSL */
-
-/* Define to 1 if the Soplex package is used */
-/* #undef COIN_HAS_SPX */
-
-/* Define to 1 if the Sym package is used */
-/* #undef COIN_HAS_SYM */
-
-/* Define to 1 if the Xpress package is used */
-/* #undef COIN_HAS_XPR */
+#ifdef SYMPHONY_BUILD
+#include "config_default.h"
+#else
+#include "config_sym_default.h"
+#endif
 
 #endif /* HAVE_CONFIG_H */
-
-#ifndef SYMPHONY_VERSION
-#define SYMPHONY_VERSION "trunk"
-#endif
 
 #endif /*__SYMPHONY_CONFIG_H__*/
