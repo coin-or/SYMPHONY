c3b4271f0571f7eb3a276f3ac230a65cc6bfed55
Ted Ralphs <ted@lehigh.edu>
2014-08-31 21:03:33 +0000

    Fixes for detecting OpenMP

--- SYMPHONY/src/LP/lp_genfunc.c
+++ SYMPHONY/src/LP/lp_genfunc.c
@@ -190,14 +190,12 @@ int process_chain(lp_prob *p)
 #ifdef COMPILE_IN_LP
 OPENMP_ATOMIC_UPDATE
       p->tm->stat.chains++;
-
 #pragma omp critical (tree_update)
 {
 OPENMP_ATOMIC_UPDATE
       p->tm->active_node_num--;
-#ifdef _OPENMP
+      //This should be unnecessary, as it is also done in purge_pruned_nodes 
       p->tm->active_nodes[p->proc_index] = NULL;
-#endif
 }
       free_node_dependent(p);
 #else
