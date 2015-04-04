3405424628c3bf80399c98b0f12d03fd5038d068
Ted Ralphs <ted@lehigh.edu>
2015-04-04 02:45:00 +0000

    Fixing ticket #144

--- SYMPHONY/src/TreeManager/tm_func.c
+++ SYMPHONY/src/TreeManager/tm_func.c
@@ -3887,18 +3887,18 @@ void sym_catch_c(int num)
    sigprocmask(SIG_SETMASK, &mask_set, &old_set);
    
    strcpy(temp, "");
-   printf("\nDo you want to abort immediately, exit gracefully (from the current solve call only), or continue? [a/e/c]: ");
-   fflush(stdout);   
+   fprintf(stderr, "\nDo you want to abort immediately, exit gracefully (from the current solve call only), or continue? [a/e/c]: ");
+   fflush(stderr);   
    fgets(temp, MAX_LINE_LENGTH, stdin);
    if(temp[1] == '\n' && (temp[0] == 'a' || temp[0] == 'A')){
-      printf("\nTerminating...\n");
-      fflush(stdout);
+      fprintf(stderr, "\nTerminating...\n");
+      fflush(stderr);
       exit(0);
    }else if(temp[1] == '\n' && (temp[0] == 'e' || temp[0] == 'E')){    
       c_count++;	    
    } else{
-      printf("\nContinuing...\n");
-      fflush(stdout);
+      fprintf(stderr, "\nContinuing...\n");
+      fflush(stderr);
       c_count = 0;      
    }
 
