43f63c2275c410e8395cfcf9cfa9e6ee4bf5ae10
Ted Ralphs <ted@lehigh.edu>
2016-01-19 00:21:40 +0000

    Adding preliminary .travis.yml

--- SYMPHONY/src/LP/lp_solver.c
+++ SYMPHONY/src/LP/lp_solver.c
@@ -2574,6 +2574,7 @@ int initial_lp_solve (LPdata *lp_data, int *iterd)
    int term = 0;
    OsiXSolverInterface  *si = lp_data->si;
 
+   //si->setHintParam(OsiDoPresolveInInitial, false, OsiHintDo);
     
    si->initialSolve();
    
@@ -3108,6 +3109,7 @@ void get_dj_pi(LPdata *lp_data)
    }
 }
 
+/*===========================================================================*/
 /*===========================================================================*/
 
 void get_slacks(LPdata *lp_data)
