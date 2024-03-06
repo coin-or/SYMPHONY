cdd7c0423f02686f5b06f17832c76672efc3f863
Yu <yux616@lehigh.edu>
2024-03-06 16:48:18 -0500

    change the condition check to compare iter number

--- src/LP/lp_solver.c
+++ src/LP/lp_solver.c
@@ -2717,8 +2717,8 @@ int dual_simplex(LPdata *lp_data, int *iterd)
 #ifdef __OSI_CLP__
       // YX: reset term if needed; Clp may return ITLIM at timeout
       int itlim_chk = -1;
-      retval = si->getIntParam(OsiMaxNumIteration, itlim_chk); 
-      if (itlim_chk >= LP_MAX_ITER){
+      retval = si->getIntParam(OsiMaxNumIteration, itlim_chk);
+      if (si->getIterationCount() < itlim_chk){
          term = LP_TIME_LIMIT;
       } 
       /* If max iterations and had switched to primal, bound is no good */
